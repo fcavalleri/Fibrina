9a3493130cfda28695d5d721e007cb056f40361b
lukecavabarrett <luca.cavallery@gmail.com>
2021-03-05 13:17:08 +0000

    Cleanup main.cpp

--- main.cpp
+++ main.cpp
@@ -9,44 +9,40 @@
 #include <iostream>
 #include <fstream>
 
-/*void DrawParticle(TParticle &i, sf::RenderWindow *app) {
-    sf::Vertex monomeroC = sf::Vertex(sf::Vector2f(i.CSite.x, i.CSite.y), sf::Color::Red);
-    app->draw(&monomeroC, 1, sf::Points);
+#define DISPLAY_SIMULATION true
 
-    sf::Vertex monomeroS = sf::Vertex(sf::Vector2f(i.LSite.x, i.LSite.y), sf::Color::White);
-    app->draw(&monomeroS, 1, sf::Points);
+namespace parameters {
+// Define system parameters
+static constexpr int N_PART = 100;
+static constexpr int GRID_LEN_X = TSite::Lx;
+static constexpr int GRID_LEN_Y = TSite::Ly;
 
-    sf::Vertex monomeroD = sf::Vertex(sf::Vector2f(i.RSite.x, i.RSite.y), sf::Color::White);
-    app->draw(&monomeroD, 1, sf::Points);
-}*/
+static constexpr int T_MAX = 800000;
+static constexpr int MSEC_WAIT = 0;
+static constexpr int VIEW = 300; //visualize every VIEW time steps. FOR REAL TIME SET TO 1
+
+static constexpr double ZY_ROT_RATE = 1;
+static constexpr double X_ROT_RATE = 0.66;
+static constexpr double TRANSL_RATE = 0.9;
+
+static constexpr double LEN_WIDHT_RATIO = 0.1;
+
+static constexpr double ACT_TRESH = 0.0012;
+static constexpr double CLO_TRESH = 0.003;
+static constexpr double DL2YL_RATE = 0;
+}
 
 int main() {
-
-// Define system parameters
-  const int N_PART = 100;
-  static constexpr int GRID_LEN_X = TSite::Lx;
-  static constexpr  int GRID_LEN_Y = TSite::Ly;
-
-  const int T_MAX = 800000;
-  const int MSEC_WAIT = 0;
-  const int VIEW = 300; //visualize every VIEW time steps. FOR REAL TIME SET TO 1
-
-  const double ZY_ROT_RATE = 1;
-  const double X_ROT_RATE = 0.66;
-  const double TRANSL_RATE = 0.9;
-
-  const double LEN_WIDHT_RATIO = 0.1;
-
-  const double ACT_TRESH = 0.0012;
-  const double CLO_TRESH = 0.003;
-  const double DL2YL_RATE = 0;
+  using namespace parameters;
 
   // Files where save analysis
-  FILE*fp1 = fopen("NfixRg","w"); //particles in aggregate and Gyration Radius
-  FILE*fp2 = fopen("nYLnDL","w"); //number of YL and DL links
+  FILE *fp1 = fopen("NfixRg", "w"); //particles in aggregate and Gyration Radius
+  FILE *fp2 = fopen("nYLnDL", "w"); //number of YL and DL links
 
-// Create the Render Window
+#if DISPLAY_SIMULATION
+  // Create the Render Window
   sf::RenderWindow app(sf::VideoMode(GRID_LEN_X, GRID_LEN_Y), "Simulazione Aggregazione Fibrina");
+#endif
 
 // Create the Lattice
   TLattice Lattice;
@@ -71,92 +67,67 @@ int main() {
 // Time Evolution
   for (int t = 0; t < T_MAX; ++t) {
 
+#if DISPLAY_SIMULATION
     // Possibility of slowing down the simulation
     usleep(MSEC_WAIT * 1000);
 
     // Clear between each time step
     app.clear();
-
+#endif
     // Ask the Lattice to Evolve all the System by a time step
     Lattice.Evolve();
 
     //Visualize Lattice every VIEW steps
     if (t % VIEW == 0) {
 
-        fprintf(fp2,"%d \t %d \t %d \n",t,Lattice.nYL,Lattice.nDL);
-        fflush(fp2);
+      fprintf(fp2, "%d \t %d \t %d \n", t, Lattice.nYL, Lattice.nDL);
+      fflush(fp2);
 
-        int Nfix{0};
-        double Rg{0};
-        double xmean{0};
-        double ymean{0};
-
+#if DISPLAY_SIMULATION
       for (auto &i : Lattice.Parts) {
         if (i.mob != TParticle::MobState::FREE) {
           //It's necessary another cycle to draw all particles, no matters if they have moved or not in this moment
-
-          //POINTS
-          /*sf::Vertex monomeroC = sf::Vertex(sf::Vector2f(i.CSite.x, i.CSite.y), sf::Color::Red);
-          app.draw(&monomeroC, 1, sf::Points);
-
-          sf::Vertex monomeroS = sf::Vertex(sf::Vector2f(i.LSite.x, i.LSite.y), sf::Color::White);
-          app.draw(&monomeroS, 1, sf::Points);
-
-          sf::Vertex monomeroD = sf::Vertex(sf::Vector2f(i.RSite.x, i.RSite.y), sf::Color::White);
-          app.draw(&monomeroD, 1, sf::Points);*/
-
-          //LINES
           sf::Vertex monomer[] =
               {
                   sf::Vertex(sf::Vector2f(i.LSite.x, i.LSite.y)),
                   sf::Vertex(sf::Vector2f(i.RSite.x, i.RSite.y))
               };
-
           app.draw(monomer, 2, sf::Lines);
+        }
+      }
+#endif
 
-          //RECTANGLES
-          /* sf::RectangleShape monomer(sf::Vector2f(3.7,3.7*LEN_WIDHT_RATIO));
-           //SetDrawMonomerSpin(&monomer);
-
-           monomer.setOrigin(monomer.getSize()*float(0.5));
-           monomer.setPosition(sf::Vector2f(i.CSite.x, i.CSite.y));
-           if (i.Spin == 0)
-           if (i.Spin == 1 ) monomer.rotate(45);
-           if (i.Spin == 2 ) monomer.rotate(135);
-           if (i.Spin == 3 ) monomer.rotate(180);
-           if (i.Spin == 4 ) monomer.rotate(225);
-           if (i.Spin == 5 ) monomer.rotate(315);
-
-           app.draw(monomer);*/
-
+      //TODO: move this computation elsewhere (e.g. in TLattice)
+      int Nfix = 0;
+      double Rg = 0;
+      double xmean = 0;
+      double ymean = 0;
+      for (auto &i : Lattice.Parts) {
+        if (i.mob != TParticle::MobState::FREE) {
           // Compute baricenter
-          xmean=xmean+i.CSite.x*0.5;
-          ymean=ymean+i.CSite.y*0.5*sqrt(3);
-          Nfix=Nfix+1;
+          xmean = xmean + i.CSite.x * 0.5;
+          ymean = ymean + i.CSite.y * 0.5 * sqrt(3);
+          Nfix = Nfix + 1;
         }
       }
 
-      xmean=xmean/Nfix;
-      ymean=ymean/Nfix;
+      xmean = xmean / Nfix;
+      ymean = ymean / Nfix;
 
       // Compute Rg^2
       for (auto &i : Lattice.Parts) {
         if (i.mob != TParticle::MobState::FREE) {
-            Rg=Rg+(i.CSite.x*0.5-xmean)*(i.CSite.x*0.5 -xmean) + (i.CSite.y*0.5*sqrt(3) -ymean)*(i.CSite.y*0.5*sqrt(3) -ymean);
+          Rg = Rg + (i.CSite.x * 0.5 - xmean) * (i.CSite.x * 0.5 - xmean)
+              + (i.CSite.y * 0.5 * sqrt(3) - ymean) * (i.CSite.y * 0.5 * sqrt(3) - ymean);
         }
       }
 
-      Rg=sqrt(Rg/Nfix);
+      Rg = sqrt(Rg / Nfix);
 
-        /*std::string filename("output_Lx");
-        filename.append(std::to_string(GRID_LEN_X)).append("_Ly")
-                .append(std::to_string(GRID_LEN_Y)).append(".txt");
-        std::ofstream output(filename);
-        output << Rg << "\t" << Nfix << "\n";*/
+      fprintf(fp1, "%e \t %d \n", Rg, Nfix);
+      fflush(fp1);
 
-        fprintf(fp1,"%e \t %d \n",Rg,Nfix);
-        fflush(fp1);
-
+#if DISPLAY_SIMULATION
       // Display Parts positions
       app.display();
       for (sf::Event event; app.pollEvent(event);)
@@ -165,8 +136,12 @@ int main() {
           app.close();
           return 0;
         }
+#endif
+
     }
   }
+#if DISPLAY_SIMULATION
   while (app.isOpen()) for (sf::Event event; app.pollEvent(event);) if (event.type == sf::Event::Closed) app.close();
+#endif
   return 0;
 }
