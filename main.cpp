2ff05a98d298bb61d72e48c9e98382c17baed65a
fcavalleri <fcavalleri@studenti.uninsubria.it>
2021-12-03 14:57:41 +0100

    last param. update for WE simulation

--- main.cpp
+++ main.cpp
@@ -20,10 +20,10 @@ static int N_PART = 1000;
 static constexpr int GRID_LEN_X = TSite::Lx;
 static constexpr int GRID_LEN_Y = TSite::Ly;
 
-static constexpr int T_MAX = 10000000;
+static constexpr int T_MAX = 300000;
 static constexpr int N_FIX_MAX = 20000;
 static constexpr int MSEC_WAIT = 0;
-static constexpr int VIEW = 500000; //visualize (and save) every VIEW time steps. FOR REAL TIME SET TO 1
+static constexpr int VIEW = 10000; //visualize (and save) every VIEW time steps. FOR REAL TIME SET TO 1
 
 #define DISPLAY_SIMULATION false
 
